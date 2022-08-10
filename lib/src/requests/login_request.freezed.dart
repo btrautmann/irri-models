eee204de46da9ed329103b6d2a8b53631cb1ec0e
Brandon Trautmann <btrautmann@users.noreply.github.com>
2022-08-10 09:23:48 -0400

    add timezone to loginrequest

--- lib/src/requests/login_request.freezed.dart
+++ lib/src/requests/login_request.freezed.dart
@@ -21,6 +21,7 @@ LoginRequest _$LoginRequestFromJson(Map<String, dynamic> json) {
 /// @nodoc
 mixin _$LoginRequest {
   String get type => throw _privateConstructorUsedError;
+  String get timezone => throw _privateConstructorUsedError;
 
   Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
   @JsonKey(ignore: true)
@@ -33,7 +34,7 @@ abstract class $LoginRequestCopyWith<$Res> {
   factory $LoginRequestCopyWith(
           LoginRequest value, $Res Function(LoginRequest) then) =
       _$LoginRequestCopyWithImpl<$Res>;
-  $Res call({String type});
+  $Res call({String type, String timezone});
 }
 
 /// @nodoc
@@ -47,11 +48,16 @@ class _$LoginRequestCopyWithImpl<$Res> implements $LoginRequestCopyWith<$Res> {
   @override
   $Res call({
     Object? type = freezed,
+    Object? timezone = freezed,
   }) {
     return _then(_value.copyWith(
       type: type == freezed
           ? _value.type
           : type // ignore: cast_nullable_to_non_nullable
+              as String,
+      timezone: timezone == freezed
+          ? _value.timezone
+          : timezone // ignore: cast_nullable_to_non_nullable
               as String,
     ));
   }
@@ -64,7 +70,7 @@ abstract class _$$_LoginRequestCopyWith<$Res>
           _$_LoginRequest value, $Res Function(_$_LoginRequest) then) =
       __$$_LoginRequestCopyWithImpl<$Res>;
   @override
-  $Res call({String type});
+  $Res call({String type, String timezone});
 }
 
 /// @nodoc
@@ -81,11 +87,16 @@ class __$$_LoginRequestCopyWithImpl<$Res>
   @override
   $Res call({
     Object? type = freezed,
+    Object? timezone = freezed,
   }) {
     return _then(_$_LoginRequest(
       type: type == freezed
           ? _value.type
           : type // ignore: cast_nullable_to_non_nullable
+              as String,
+      timezone: timezone == freezed
+          ? _value.timezone
+          : timezone // ignore: cast_nullable_to_non_nullable
               as String,
     ));
   }
@@ -94,17 +105,19 @@ class __$$_LoginRequestCopyWithImpl<$Res>
 /// @nodoc
 @JsonSerializable()
 class _$_LoginRequest implements _LoginRequest {
-  _$_LoginRequest({required this.type});
+  _$_LoginRequest({required this.type, required this.timezone});
 
   factory _$_LoginRequest.fromJson(Map<String, dynamic> json) =>
       _$$_LoginRequestFromJson(json);
 
   @override
   final String type;
+  @override
+  final String timezone;
 
   @override
   String toString() {
-    return 'LoginRequest(type: $type)';
+    return 'LoginRequest(type: $type, timezone: $timezone)';
   }
 
   @override
@@ -112,13 +125,16 @@ class _$_LoginRequest implements _LoginRequest {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$_LoginRequest &&
-            const DeepCollectionEquality().equals(other.type, type));
+            const DeepCollectionEquality().equals(other.type, type) &&
+            const DeepCollectionEquality().equals(other.timezone, timezone));
   }
 
   @JsonKey(ignore: true)
   @override
-  int get hashCode =>
-      Object.hash(runtimeType, const DeepCollectionEquality().hash(type));
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(type),
+      const DeepCollectionEquality().hash(timezone));
 
   @JsonKey(ignore: true)
   @override
@@ -132,13 +148,17 @@ class _$_LoginRequest implements _LoginRequest {
 }
 
 abstract class _LoginRequest implements LoginRequest {
-  factory _LoginRequest({required final String type}) = _$_LoginRequest;
+  factory _LoginRequest(
+      {required final String type,
+      required final String timezone}) = _$_LoginRequest;
 
   factory _LoginRequest.fromJson(Map<String, dynamic> json) =
       _$_LoginRequest.fromJson;
 
   @override
   String get type;
+  @override
+  String get timezone;
   @override
   @JsonKey(ignore: true)
   _$$_LoginRequestCopyWith<_$_LoginRequest> get copyWith =>
